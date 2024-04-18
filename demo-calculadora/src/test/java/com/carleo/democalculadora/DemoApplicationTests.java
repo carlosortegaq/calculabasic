9209ec1853da7d3c311d4ab17fd481d1b7f124fb
Carlos Ortega <carlosortegaq@gmail.com>
2024-04-17 21:55:50 -0500

    Se ajusta el test de pruebas unitarias

--- demo-calculadora/src/test/java/com/carleo/democalculadora/DemoApplicationTests.java
+++ demo-calculadora/src/test/java/com/carleo/democalculadora/DemoApplicationTests.java
@@ -17,9 +17,9 @@ class DemoApplicationTests {
 	@Test
 	void test() {
 		calculadora = new Calculadora();
-		calculadora.setNumero1(10);
+		calculadora.setNumero1(20);
 		calculadora.setNumero2(10);
-		Integer resultadoEsperado = 1;
+		Integer resultadoEsperado = 2;
 		Integer resultado;
 		try {
 			resultado = calculadora.dividir();
